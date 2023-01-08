39e9a0560d329b0ddea46f0e4e9d7733270ab46b
Yoooi <kamos21@protonmail.com>
2023-01-08 17:32:44 +0100

    Add samples

---
+++ samples/VibrateAllDevices/Program.cs
@@ -0,0 +1,20 @@
+﻿using Buttplug.SystemTextJson;
+using Buttplug;
+
+var cancellationToken = CancellationToken.None;
+var converter = new ButtplugSystemTextJsonConverter();
+await using var client = new ButtplugClient("Buttplug.Net", converter);
+
+client.DeviceAdded += (_, e) => Console.WriteLine($"Device added: {e}");
+client.DeviceRemoved += (_, e) => Console.WriteLine($"Device removed: {e}");
+client.ScanningFinished += (_, e) => Console.WriteLine("Scanning finished");
+client.ErrorReceived += (_, e) => Console.WriteLine($"Error received: {e}");
+client.Disconnected += (_, e) => Console.WriteLine("Disconnected");
+
+await client.ConnectAsync(new Uri("ws://127.0.0.1:12345/"), cancellationToken);
+
+foreach (var device in client.Devices)
+    await device.ScalarAsync(1, ActuatorType.Vibrate, cancellationToken);
+
+await Task.Delay(1000);
+await client.StopAllDevicesAsync(cancellationToken);

8f909a2fd8f852be81a7dfbf9a7ff0140c5648c1
Yoooi <kamos21@protonmail.com>
2023-01-11 23:24:58 +0100

    Update VibrateAllDevices sample

--- samples/VibrateAllDevices/Program.cs
+++ samples/VibrateAllDevices/Program.cs
@@ -15,11 +15,16 @@ await client.ConnectAsync(new Uri("ws://127.0.0.1:12345/"), cancellationToken);
 Console.WriteLine("Connected");
 
 await client.StartScanningAsync(cancellationToken);
-await Task.Delay(1000);
+await Task.Delay(2500);
 await client.StopScanningAsync(cancellationToken);
 
 foreach (var device in client.Devices)
     await device.ScalarAsync(1, ActuatorType.Vibrate, cancellationToken);
+
+await Task.Delay(1000);
+
+foreach (var actuator in client.Devices.SelectMany(d => d.GetActuators<ButtplugDeviceScalarActuator>(ActuatorType.Vibrate)))
+    await actuator.ScalarAsync(0.5, cancellationToken);
 
 await Task.Delay(1000);
 await client.StopAllDevicesAsync(cancellationToken);

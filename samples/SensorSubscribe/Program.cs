4c59b3a266678a1a204f8ec165c19dd56d5a79df
Yoooi <kamos21@protonmail.com>
2023-01-11 23:29:18 +0100

    Add SensorSubscribe sample

---
+++ samples/SensorSubscribe/Program.cs
@@ -0,0 +1,30 @@
+﻿using Buttplug.SystemTextJson;
+using Buttplug;
+
+var cancellationToken = CancellationToken.None;
+var converter = new ButtplugSystemTextJsonConverter();
+await using var client = new ButtplugClient("Buttplug.Net", converter);
+
+client.DeviceAdded += (_, e) => Console.WriteLine($"Device added: {e}");
+client.DeviceRemoved += (_, e) => Console.WriteLine($"Device removed: {e}");
+client.ScanningFinished += (_, e) => Console.WriteLine("Scanning finished");
+client.UnhandledException += (_, e) => Console.WriteLine($"Error received: {e}");
+client.Disconnected += (_, e) => Console.WriteLine("Disconnected");
+
+await client.ConnectAsync(new Uri("ws://127.0.0.1:12345/"), cancellationToken);
+Console.WriteLine("Connected");
+
+await client.StartScanningAsync(cancellationToken);
+await Task.Delay(2500);
+await client.StopScanningAsync(cancellationToken);
+
+var sensor = client.Devices.SelectMany(d => d.SubscribeSensors).FirstOrDefault();
+if (sensor != null)
+{
+    var subscription = await sensor.SubscribeAsync((_, i, d) => Console.WriteLine($"Sensor {i} data: \"{string.Join(", ", d)}\""), cancellationToken);
+    await Task.Delay(10000);
+    await subscription.UnsubscribeAsync(cancellationToken);
+}
+
+await Task.Delay(1000);
+await client.StopAllDevicesAsync(cancellationToken);
